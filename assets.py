bd05447a848ed35507e89dd68fc26c926a5da2ab
Sarvjeet Singh <sarvjeet@gmail.com>
2021-04-14 02:35:54 -0700

    Add I and EE bonds (no tests).

--- assets.py
+++ assets.py
@@ -3,7 +3,9 @@
 All these assets should implement the lakshmi.Asset top-level interface.
 """
 
+import datetime
 import lakshmi
+import re
 import requests
 import yfinance
 
@@ -66,3 +68,64 @@ class VanguardFund(lakshmi.Asset):
 
   def Value(self):
     return self.shares * self.price
+
+
+class _TresuryBonds(lakshmi.Asset):
+  def __init__(self, series, class2ratio):
+    if series != 'EE' and series != 'I':
+      raise lakshmi.ValidationError('Only I and EE series bonds are supported')
+
+    self.series = series
+    super().__init__(class2ratio)
+    self.value = 0
+    self.bonds = []
+
+  def AddBond(self, issue_date, denom):
+    rate, bond_value = self._GetBondInfo(issue_date, denom)
+    self.value += bond_value
+    self.bonds.append([issue_date, denom, rate, bond_value])
+
+  def _GetBondInfo(self, issue_date, denom):
+    scale = denom / 1000  # TD website doesn't support some denominations for electronic bonds.
+    if self.series == 'EE':
+      ## EE Bonds returned are half the value (I guess TD website assumes paper bonds)
+      scale *= 2
+
+    data = {
+      "RedemptionDate" : datetime.datetime.now().strftime('%m/%Y'),
+      "Series" : self.series,
+      "Denomination" : '1000',
+      "IssueDate" : issue_date,
+      "btnAdd.x" : "CALCULATE"
+    };
+
+    req = requests.post('http://www.treasurydirect.gov/BC/SBCPrice', data=data)
+    req.raise_for_status()
+
+    # float(re.sub('\n|<[^>]+>', '', re.findall('\n<td>.*</td>', r.text)[7]))
+    ret_vals = re.findall('\n<td>.*</td>', req.text)
+    rate = re.sub('\n|<[^>]+>', '', ret_vals[6])
+    value = float(re.sub('\n|\$|,|<[^>]+>', '', ret_vals[7]))
+
+    return rate, value * scale
+
+  def Value(self):
+    return self.value
+
+  def ListBonds(self):
+    return self.bonds
+
+
+class IBonds(_TresuryBonds):
+  def __init__(self, class2ratio):
+    super().__init__('I', class2ratio)
+
+  def Name(self):
+    return 'I Bonds'
+
+class EEBonds(_TresuryBonds):
+  def __init__(self, class2ratio):
+    super().__init__('EE', class2ratio)
+
+  def Name(self):
+    return 'EE Bonds'
