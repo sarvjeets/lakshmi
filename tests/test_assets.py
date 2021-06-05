217f2978f0b7eec40f0b8c5448ec6f495e3e9443
Sarvjeet Singh <sarvjeet@gmail.com>
2021-06-04 19:11:29 -0700

    Cleanup: Move data from test_assets.py into data files.

--- tests/test_assets.py
+++ tests/test_assets.py
@@ -1,6 +1,8 @@
 """Tests for lakshmi.assets module."""
+import json
 import lakshmi.assets as assets
 import lakshmi.cache
+import os
 import unittest
 from unittest.mock import MagicMock, patch
 
@@ -8,6 +10,9 @@ from unittest.mock import MagicMock, patch
 class AssetsTest(unittest.TestCase):
   def setUp(self):
     lakshmi.cache.CACHE_DIR = None  # Disable caching.
+    self.data_dir = os.path.join(
+      os.path.dirname(os.path.abspath(__file__)),
+      'data')
 
   def testDictManualAssetWithWhatIf(self):
     manual_asset = assets.ManualAsset('Cash', 100.5, {'Fixed Income': 1.0})
@@ -94,7 +99,11 @@ class AssetsTest(unittest.TestCase):
   @patch('requests.get')
   def testVanguardFundsName(self, MockGet):
     MockReq = MagicMock()
-    MockReq.json.return_value = {"fundProfile":{"fundId":"1884","citFundId":"7555","instrumentId":27075102,"shortName":"Inst Tot Bd Mkt Ix Tr","longName":"Vanguard Institutional Total Bond Market Index Trust","inceptionDate":"2016-06-24T00:00:00-04:00","newspaperAbbreviation":"VanTBdMIxInsSel     ","style":"Bond Funds","type":"Bond Funds","category":"Intermediate-Term Bond","customizedStyle":"Bond - Inter-term Investment","fixedIncomeInvestmentStyleId":"2","fixedIncomeInvestmentStyleName":"Intermediate-term Treasury","secDesignation":"","maximumYearlyInvestment":"","expenseRatio":"0.0100","expenseRatioAsOfDate":"2020-04-28T00:00:00-04:00","isInternalFund":True,"isExternalFund":False,"isMutualFund":True,"isETF":False,"isVLIP":False,"isVVAP":False,"is529":False,"hasAssociatedInvestorFund":True,"hasMoreThan1ShareClass":True,"isPESite":True,"fundFact":{"isActiveFund":True,"isClosed":False,"isClosedToNewInvestors":False,"isFundOfFunds":False,"isMSCIIndexedFund":False,"isIndex":True,"isLoadFund":False,"isMoneyMarket":False,"isBond":True,"isBalanced":False,"isStock":False,"isInternational":False,"isMarketNeutralFund":False,"isInternationalStockFund":False,"isInternationalBalancedFund":False,"isDomesticStockFund":False,"isTaxable":True,"isTaxExempt":False,"isTaxManaged":False,"isTaxableBondFund":True,"isTaxExemptBondFund":False,"isTaxExemptMoneyMarketFund":False,"isTaxSensitiveFund":True,"isSpecialtyStockFund":False,"isHybridFund":False,"isGlobal":False,"isManagedPayoutFund":False,"isGNMAFund":False,"isInvestorShare":False,"isAdmiralShare":False,"isInstitutionalShare":False,"isAdmiralFund":False,"isStableValueFund":False,"isCompanyStockFund":False,"isREITFund":False,"isVariableInsuranceFund":False,"isComingledTrustFund":False,"isConvertibleFund":False,"isAssetAllocationFund":False,"isStateMunicipalBond":False,"isNationalMunicipalBond":False,"isQualifiedOnly":False,"isPreciousMetalsFund":False,"mIsVIPSFund":False,"isSectorSpecific":False,"hasOtherIndex":False,"isTargetRetirementFund":False,"isRetirementSavingsTrustFund":False,"isNon40ActFund":True,"isUnfundedFund":False,"isCreditSuisseFund":False,"isKaiserFund":False,"isFundAccessFund":False,"isFundTransferableToVGI":False,"hasTransactionFee":False,"isNTFFund":False,"hasMoreThan1ShareClass":False,"isOpenToFlagship":False,"isOpenToFlagshipPlus":False,"isCitFund":True,"isAcctType15Fund":False,"isEtfOfEtfs":False,"isStandaloneEtf":False},"associatedFundIds":{"investorFundId":"0084","admiralFundId":"0584","etfFundId":"0928","institutionalFundId":"0222","institutionalPlusFundId":"0850"},"fundCategory":{"customizedHighCategoryName":"Bond - Inter-term Investment","high":{"type":"HIGH","id":3,"name":"Bond Funds"},"mid":{"type":"MID","id":31,"name":"Bond Funds"},"low":{"type":"LOW","id":3105,"name":"Intermediate-Term Bond"}},"largeTransactionAmount":25000000,"qualifiedTransactionAmount":50000000,"minimumInitialInvestment":3.0E9,"signalFundFlag":False},"historicalReturn":{"percent":"8.85","startDate":"1972-12-31T00:00:00-05:00","endDate":"2011-12-31T00:00:00-05:00"}}
+
+    data_file_path = os.path.join(self.data_dir, 'profile.json')
+    with open(data_file_path) as data_file:
+      MockReq.json.return_value = json.load(data_file)
+
     MockGet.return_value = MockReq
 
     fund = assets.VanguardFund(7555, 10, {'All': 1.0})
@@ -108,7 +117,10 @@ class AssetsTest(unittest.TestCase):
   @patch('requests.get')
   def testVanguardFundsValue(self, MockGet):
     MockReq = MagicMock()
-    MockReq.json.return_value = {"currentPrice":{"yield":{"hasDisclaimer":False},"dailyPrice":{"regular":{"asOfDate":"2021-04-15T00:00:00-04:00","price":"116.66","priceChangeAmount":"0.52","priceChangePct":"0.45","currOrPrmlFlag":"CURR","currOrPrmlValue":"Price"}},"highLow":{"regular":{"highDate":"2020-08-06T00:00:00-04:00","highPrice":"120.210000","lowDate":"2021-03-18T00:00:00-04:00","lowPrice":"114.970000","spreadPrice":"5.240000","spreadPct":"4.56","hasMultipleHighDates":False,"hasMultipleLowDates":False,"highDates":[{}],"lowDates":[{}]}}},"historicalPrice":{"isMultiYears":False,"nav":[{"item":[{"asOfDate":"2021-04-15T00:00:00-04:00","price":"116.66"},{"asOfDate":"2021-04-14T00:00:00-04:00","price":"116.14"},{"asOfDate":"2021-04-13T00:00:00-04:00","price":"116.24"},{"asOfDate":"2021-04-12T00:00:00-04:00","price":"115.93"},{"asOfDate":"2021-04-09T00:00:00-04:00","price":"116.01"},{"asOfDate":"2021-04-08T00:00:00-04:00","price":"116.11"},{"asOfDate":"2021-04-07T00:00:00-04:00","price":"115.90"},{"asOfDate":"2021-04-06T00:00:00-04:00","price":"115.99"},{"asOfDate":"2021-04-05T00:00:00-04:00","price":"115.67"},{"asOfDate":"2021-04-01T00:00:00-04:00","price":"115.86"}]}],"marketPrice":[{}]}}
+
+    data_file_path = os.path.join(self.data_dir, 'price.json')
+    with open(data_file_path) as data_file:
+      MockReq.json.return_value = json.load(data_file)
     MockGet.return_value = MockReq
 
     fund = assets.VanguardFund(7555, 10, {'All': 1.0})
@@ -133,24 +145,9 @@ class AssetsTest(unittest.TestCase):
   @patch('requests.post')
   def testIBonds(self, MockPost, MockDate):
     MockReq = MagicMock()
-    MockReq.text = """Stuff
-    <td>More stuff</td>
-<div id="bogus">
-<td class="lft">NA</td>
-<td class="se">I</td>
-<td>$1,000</td>
-<td>03/2020</td>
-<td>05/2021</td>
-<td>03/2050</td>
-<td>$1,000.00</td>
-<td>$15.60</td>
-<td>1.88%</td>
-<td><strong>$1,015.60</strong></td>
-<td class="nt"><a href="#nte">P5</a></td>
-<td class="rgt"><input class="linkbutton" type="submit" name="btnDel0.x" value="REMOVE" /></td>
-</tr>
-... and more stuff
-"""
+    html_file_path = os.path.join(self.data_dir, 'SBCPrice-I.html')
+    with open(html_file_path) as html_file:
+      MockReq.text = html_file.read()
     MockPost.return_value = MockReq
     MockDate.now.strftime.return_value = '04/2021'
 
@@ -190,24 +187,9 @@ class AssetsTest(unittest.TestCase):
   @patch('requests.post')
   def testEEBonds(self, MockPost, MockDate):
     MockReq = MagicMock()
-    MockReq.text = """Stuff
-    <td>More stuff</td>
-<div id="bogus">
-<td class="lft">NA</td>
-<td class="se">EE</td>
-<td>$1,000</td>
-<td>03/2020</td>
-<td>05/2021</td>
-<td>03/2050</td>
-<td>$500.00</td>
-<td>$0.40</td>
-<td>0.10%</td>
-<td><strong>$500.40</strong></td>
-<td class="nt"><a href="#nte">P5</a></td>
-<td class="rgt"><input class="linkbutton" type="submit" name="btnDel0.x" value="REMOVE" /></td>
-</tr>
-... and more stuff
-"""
+    html_file_path = os.path.join(self.data_dir, 'SBCPrice-EE.html')
+    with open(html_file_path) as html_file:
+      MockReq.text = html_file.read()
     MockPost.return_value = MockReq
     MockDate.now.strftime.return_value = '04/2021'
 
