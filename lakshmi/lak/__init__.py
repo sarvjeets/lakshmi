93cf647981f0cf7fbe2285cce2f6f09416d1f43e
Sarvjeet Singh <sarvjeet@gmail.com>
2021-06-13 00:00:20 -0700

    LakConfig -> LakContext and moved seperator into context class

--- lakshmi/lak/__init__.py
+++ lakshmi/lak/__init__.py
@@ -6,19 +6,20 @@ from pathlib import Path
 import yaml
 
 
-class LakConfig:
-    LAKRC = '.lakrc'
-
+class LakContext:
     def _ReturnConfig(self):
-        lakrcfile = Path.home() / self.LAKRC
+        lakrcfile = Path(self.lakrc).expanduser()
 
         if not lakrcfile.exists():
             return {}
         config = yaml.load(lakrcfile.read_text(), Loader=yaml.SafeLoader)
         return config
 
-    def __init__(self):
+    def __init__(self, lakrc='~/.lakrc'):
+        self.lakrc = lakrc
+        self.continued = False
         config = self._ReturnConfig()
+
         portfolio_filename = config.pop('portfolio', '~/portfolio.yaml')
         self.portfolio_filename = str(Path(portfolio_filename).expanduser())
         self._portfolio = Portfolio.Load(self.portfolio_filename)
@@ -33,6 +34,12 @@ class LakConfig:
         assert len(config) == 0, (
             f'Extra entries found in config file: {list(config.keys())}')
 
+    def Separator(self):
+        if self.continued:
+            click.echo('\n')
+        # Set it up so that separator is printed for the next command.
+        self.continued = True
+
     def Portfolio(self):
         return self._portfolio
 
@@ -40,18 +47,7 @@ class LakConfig:
         self._portfolio.Save(self.portfolio_filename)
 
 
-lakconfig = LakConfig()
-continued = False
-
-
-def Separator():
-    """Prints separator if multiple commands are chained."""
-    global continued
-    if continued:
-        click.echo('\n')
-    # Set it up so that separator is printed for the next command.
-    continued = True
-
+lakctx = None
 
 @click.group()
 @click.option('--force-refresh/--no-force-refresh',
@@ -60,6 +56,9 @@ def Separator():
         help='If set, fetches new data instead of using cached data.')
 def lak(force_refresh):
     lakshmi.cache.set_force_refresh(force_refresh)
+    global lakctx
+    if not lakctx:
+        lakctx = LakContext()
 
 
 @lak.group(chain=True)
@@ -70,9 +69,9 @@ def list():
 @list.command()
 def total():
     """Prints the total value."""
-    Separator()
-    global lakconfig
-    portfolio = lakconfig.Portfolio()
+    global lakctx
+    lakctx.Separator()
+    portfolio = lakctx.Portfolio()
     click.echo(
         Table(2, coltypes=['str', 'dollars']).AddRow(
             ['Total Assets', portfolio.TotalValue()]).String())
@@ -81,9 +80,9 @@ def total():
 @list.command()
 def al():
     """Prints the Asset Location."""
-    Separator()
-    global lakconfig
-    portfolio = lakconfig.Portfolio()
+    global lakctx
+    lakctx.Separator()
+    portfolio = lakctx.Portfolio()
     click.echo(portfolio.AssetLocation().String())
 
 
@@ -97,9 +96,9 @@ def al():
         'should sum to one.')
 def aa(compact, asset_class):
     """Prints the Asset Allocation."""
-    Separator()
-    global lakconfig
-    portfolio = lakconfig.Portfolio()
+    global lakctx
+    lakctx.Separator()
+    portfolio = lakctx.Portfolio()
     if asset_class:
         assert compact, ('--no-compact is only supported when --asset-class'
                 'is not specified.')
@@ -121,9 +120,9 @@ def aa(compact, asset_class):
         'assets that support it)')
 def assets(short_name, quantity):
     """Prints Assets and their current values."""
-    Separator()
-    global lakconfig
-    portfolio = lakconfig.Portfolio()
+    global lakctx
+    lakctx.Separator()
+    portfolio = lakctx.Portfolio()
     click.echo(portfolio.Assets(short_name=short_name, quantity=quantity).String())
 
 @lak.command(context_settings={"ignore_unknown_options": True})
@@ -137,15 +136,15 @@ def assets(short_name, quantity):
 def whatif(asset, account, reset, delta):
     """Run hypothetical what if scenarios."""
     # Sanity check the flags.
-    global lakconfig
-    portfolio = lakconfig.Portfolio()
+    global lakctx
+    portfolio = lakctx.Portfolio()
 
     if reset:
         assert asset is None and account is None and delta is None, (
                 'Can\'t specify any other flags/arguments when --reset is '
                 'specified')
         portfolio.ResetWhatIfs()
-        lakconfig.SavePortfolio()
+        lakctx.SavePortfolio()
         return
 
     assert delta is not None, ('Must specify a value (delta) to add/subtract '
@@ -154,14 +153,14 @@ def whatif(asset, account, reset, delta):
     if asset is None and account is not None:  # Delta is applied to Account
         account_name = portfolio.GetAccountNameBySubStr(account)
         portfolio.WhatIfAddCash(account_name, delta)
-        lakconfig.SavePortfolio()
+        lakctx.SavePortfolio()
         return
 
     if asset is not None:
         account_name, asset_name = portfolio.GetAssetNameBySubStr(
                 account if account is not None else '', asset)
         portfolio.WhatIf(account_name, asset_name, delta)
-        lakconfig.SavePortfolio()
+        lakctx.SavePortfolio()
         return
 
     raise click.ClickException('One of asset or account must be provided.')
