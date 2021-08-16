c0a6600d3b653c30b79c44ee26e24e10d0118adf
Sarvjeet Singh <sarvjeet@gmail.com>
2021-08-16 16:52:06 -0700

    Add classifier section

--- setup.py
+++ setup.py
@@ -15,6 +15,12 @@ setup(
     long_description_content_type='text/markdown',
     url='https://github.com/sarvjeets/lakshmi',
     platforms='any',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ],
     py_modules=['lakshmi'],
     packages=find_packages(exclude=['tests', 'tests.*']),
     include_package_data=True,
