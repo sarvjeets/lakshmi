3577cfce7c505d197fcca825bbf877dfed2a0414
Sarvjeet Singh <sarvjeet@gmail.com>
2021-11-22 18:40:52 -0800

    Complete implementation of Checkpoint and Timeline
    Also added simple tests.

--- lakshmi/performance.py
+++ lakshmi/performance.py
@@ -1,49 +1,162 @@
 """This module contains all classes and functions related to checkpointing
 and computing portfolio's performance."""
 
-from datetime import date
+from bisect import bisect
+from datetime import datetime
+
+from lakshmi import utils
 
 
 class Checkpoint:
-    """Class representing a single checkpoint of the portfolio."""
+    """Class representing a single checkpoint of the portfolio. Each checkpoint
+    represents a single day. The checkpoint contains the portfolio value, and
+    money inflows and outflows on that day.
+    """
 
-    def __init__(self, portfolio_value,
-                 checkpoint_date=date.today().isoformat()):
+    def __init__(self, checkpoint_date, portfolio_value, inflow=0, outflow=0):
         """Constructs a new checkpoint for the given date.
 
         Args:
-            checkpoint_date: Date in 'YYYY-MM-DD' format.
-            portfolio_value: The current value of the portfolio.
+            checkpoint_date: Date in 'YYYY/MM/DD' format.
+            portfolio_value: The value of the portfolio on that date.
+            inflow: The amount of money flowing into the portfolio on date.
+            outflow: The amount of money flowing out of the portfolio on date.
         """
-        self._date = checkpoint_date
-        assert portfolio_value >= 0.0
+        self._date = utils.validate_date(checkpoint_date)
+
+        assert portfolio_value >= 0, 'Portfolio value must be non-negative'
+        assert inflow >= 0, 'Inflow must be non-negative'
+        assert outflow >= 0, 'Outflow must be non-negative'
+
         self._portfolio_value = portfolio_value
+        self._inflow = inflow
+        self._outflow = outflow
 
     def get_date(self):
-        """Returns date of this checkpoint in "YYYY-MM-DD" format."""
+        """Returns date of this checkpoint in 'YYYY/MM/DD' format."""
         return self._date
 
     def get_portfolio_value(self):
         """Returns the checkpoint's portfolio value."""
         return self._portfolio_value
 
-    def merge(self, other):
-        """Returns a checkpoint by merging other into this object."""
-        # Right now this doesn't do much.
-        return self
+    def get_inflow(self):
+        """Returns the money flowing in."""
+        return self._inflow
+
+    def get_outflow(self):
+        """Returns the money flowing out."""
+        return self._outflow
 
 
 class Timeline:
     """Class representing a collection of checkpoints."""
 
-    def __init__(self, checkpoints=[]):
+    def __init__(self, checkpoints):
         """Returns a new object given a list of checkpoints."""
-        self._checkpoints = checkpoints
-        # TODO: Do some validation.
+        assert len(checkpoints) > 0
 
-    def return_checkpoints(self, begin=None, end=None):
-        """Return all the checkpoints between begin and end."""
-        pass
+        self._checkpoints = {}
+        self._dates = []
+        for cp in checkpoints:
+            cp_date = cp.get_date()
+            assert cp_date not in self._dates, (
+                'Cannot have two checkpoints with the same date')
+            self._dates.append(cp_date)
+            self._checkpoints[cp_date] = cp
+        self._dates.sort()
+
+    def has_checkpoint(self, date):
+        """Retuns true iff there is a checkpoint for date."""
+        return utils.validate_date(date) in self._checkpoints
+
+    def begin(self):
+        """Returns the beginnning date of this timeline."""
+        return self._dates[0]
+
+    def end(self):
+        """Returns the end date of this timeline."""
+        return self._dates[-1]
+
+    def covers(self, date):
+        """Returns true if date is within the timeline."""
+        date = utils.validate_date(date)
+        return (date >= self.begin() and date <= self.end())
+
+    @classmethod
+    def _interpolate_checkpoint(cls, date, checkpoint1, checkpoint2):
+        """Given checkpoints 1 and 2, returns new checkpoint for date."""
+        date1 = datetime.strptime(checkpoint1.get_date(), '%Y/%m/%d')
+        date2 = datetime.strptime(checkpoint2.get_date(), '%Y/%m/%d')
+        given_date = datetime.strptime(date, '%Y/%m/%d')
+        val1 = checkpoint1.get_portfolio_value()
+        val2 = (checkpoint2.get_portfolio_value()
+                - checkpoint2.get_inflow()
+                + checkpoint2.get_outflow())
+
+        interpolated_value = val1 + (val2 - val1) * (
+            (given_date - date1) / (date2 - date1))
+        return Checkpoint(date, interpolated_value)
+
+    def get_checkpoint(self, date, interpolate=False):
+        """Returns checkpoint for a given date.
+
+        This function will return the checkpoint for date if it already exists
+        in the Timeline. If there is no checkpoint for date, this function
+        throws an error if interpolate is False. If interpolate is set to True
+        this function will linearly interpolate the portfolio values around
+        the given date and return a newly created checkpoint with that
+        calculated value.
+
+        Args:
+            date: The date (in 'YYYY/MM/DD' format) for which to return the
+            checkpoint.
+            interpolate: If True, computes and returns a checkpoint for a date
+            even if one doesn't exists in the Timeline.
+        Returns:
+            Checkpoint object corresponding to date.
+        Raises:
+            AssertionError, if interpolate is False and there is no checkpoint
+            for date.
+            AssertError, If date is not without the range of checkpoints in
+            this timeline.
+        """
+        date = utils.validate_date(date)
+
+        if self.has_checkpoint(date):
+            return self._checkpoints[date]
+
+        # date is not one of the saved checkpoints...
+
+        assert interpolate, f'{date} is not one of the saved checkpoints.'
+
+        # ... and it's OK to interpolate
+
+        assert self.covers(date), (
+            f'{date} is not in the range of the saved checkpoints. '
+            f'Begin={self.begin()}, End={self.end()}')
+
+        pos = bisect(self._dates, date)
+        return Timeline._interpolate_checkpoint(
+            date,
+            self._checkpoints[self._dates[pos - 1]],
+            self._checkpoints[self._dates[pos]])
+
+    def insert_checkpoint(self, checkpoint):
+        """Inserts checkpoint to the timeline."""
+        date = checkpoint.get_date()
+        assert not self.has_checkpoint(date), (
+            'Cannot insert two checkpoints with the same date.')
+        pos = bisect(self._dates, date)
+        self._dates.insert(pos, date)
+        self._checkpoints[date] = checkpoint
+
+    def delete_checkpoint(self, date):
+        """Removes checkpoint corresponding to date."""
+        date = utils.validate_date(date)
+        assert date in self._checkpoints
+        self._checkpoints.pop(date)
+        self._dates.remove(date)
 
 
 class Performance:
--- tests/test_performance.py
+++ tests/test_performance.py
@@ -2,18 +2,79 @@
 
 import unittest
 
-from lakshmi.performance import Checkpoint
+from lakshmi.performance import Checkpoint, Timeline
 
 
 class PerformanceTest(unittest.TestCase):
 
     def test_checkpoint(self):
-        c1 = Checkpoint(100)  # Default constructor.
-        self.assertEquals(100, c1.get_portfolio_value())
-        c = Checkpoint(200, '2020-11-11')
-        self.assertEquals(200, c.get_portfolio_value())
-        self.assertEquals('2020-11-11', c.get_date())
+        c = Checkpoint('2020/11/11', 100)  # Default constructor.
+        self.assertEqual(100, c.get_portfolio_value())
+        self.assertEqual('2020/11/11', c.get_date())
 
-        c.merge(c1)
-        self.assertEquals(200, c.get_portfolio_value())
-        self.assertEquals('2020-11-11', c.get_date())
+        c = Checkpoint('2020/1/1', 200, inflow=100, outflow=50)
+        self.assertEqual(200, c.get_portfolio_value())
+        self.assertEqual('2020/01/01', c.get_date())
+        self.assertEqual(100, c.get_inflow())
+        self.assertEqual(50, c.get_outflow())
+
+    def test_empty_timeline(self):
+        with self.assertRaises(AssertionError):
+            Timeline([])
+
+    def test_single_entry_timeline(self):
+        cp = Checkpoint('2021/1/1', 100.0)
+        timeline = Timeline([cp])
+
+        self.assertTrue(timeline.has_checkpoint('2021/01/1'))
+        self.assertFalse(timeline.has_checkpoint('2021/01/02'))
+
+        self.assertEqual('2021/01/01', timeline.begin())
+        self.assertEqual('2021/01/01', timeline.end())
+
+        self.assertFalse(timeline.covers('2020/12/31'))
+        self.assertTrue(timeline.covers('2021/1/1'))
+
+        self.assertEqual(100, timeline.get_checkpoint('2021/01/1')
+                         .get_portfolio_value())
+        self.assertEqual(100, timeline.get_checkpoint('2021/01/1', True)
+                         .get_portfolio_value())
+        with self.assertRaises(AssertionError):
+            timeline.get_checkpoint('2021/01/02')
+        with self.assertRaises(AssertionError):
+            timeline.get_checkpoint('2021/01/02', True)
+
+        with self.assertRaises(AssertionError):
+            timeline.insert_checkpoint(cp)
+
+        cp1 = Checkpoint('2020/1/1', 200)
+        timeline.insert_checkpoint(cp1)
+        self.assertEqual('2020/01/01', timeline.begin())
+
+    def test_timeline(self):
+        checkpoints = [
+            Checkpoint('2021/1/1', 100), Checkpoint('2021/3/1', 500),
+            Checkpoint('2021/1/31', 300, inflow=150, outflow=50)]
+        timeline = Timeline(checkpoints)
+
+        self.assertTrue(timeline.has_checkpoint('2021/01/1'))
+        self.assertFalse(timeline.has_checkpoint('2021/01/02'))
+
+        self.assertEqual('2021/01/01', timeline.begin())
+        self.assertEqual('2021/03/01', timeline.end())
+
+        self.assertFalse(timeline.covers('2020/12/31'))
+        self.assertTrue(timeline.covers('2021/1/1'))
+        self.assertTrue(timeline.covers('2021/1/2'))
+        self.assertTrue(timeline.covers('2021/3/1'))
+
+        self.assertEqual(300, timeline.get_checkpoint('2021/01/31')
+                         .get_portfolio_value())
+        self.assertEqual(300, timeline.get_checkpoint('2021/01/31', True)
+                         .get_portfolio_value())
+
+        with self.assertRaises(AssertionError):
+            timeline.get_checkpoint('2021/01/15')
+
+        self.assertEqual(150, timeline.get_checkpoint('2021/01/16', True)
+                         .get_portfolio_value())
